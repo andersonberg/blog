3842d64f7b33e95afddccaf7971cd5dcdc2395c1
Anderson Berg <andersonberg@gmail.com>
2017-03-22 23:42:36 -0300

    update config

--- pelicanconf.py
+++ pelicanconf.py
@@ -50,6 +50,6 @@ THEME = "clean-blog"
 COLOR_SCHEME_CSS = 'monokai.css'
 
 
-DISQUS_SITENAME = 'pythonize'
+DISQUS_SITENAME = 'pythonize.org'
 GOOGLE_ANALYTICS = 'UA-25538784-1'
 TWITTER_USERNAME = 'berg_pe'
