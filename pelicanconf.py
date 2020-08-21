0f8af8ceb117d818da10f93cc4aa55ba2f7d8f6f
Anderson Berg <andersonberg@gmail.com>
2020-08-20 23:10:29 -0300

    corrige deploy

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,7 +41,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-STATIC_PATHS = ['images']
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 DISPLAY_CATEGORIES_ON_MENU = False
 DELETE_OUTPUT_DIRECTORY = False
